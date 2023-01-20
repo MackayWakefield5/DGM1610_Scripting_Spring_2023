8f8d4523effa59962ac183662c28469271c4a799
Mackay Wakefield <10813992@uvu.edu>
2023-01-19 17:41:56 -0700

    Added variable and Move

---
+++ First Project/Assets/Scripts/Variables.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    public string herosName = "Batman";
+
+    public int herosAge = 30;
+
+    public float herosHeight = 6.2;
+
+    public double herosNetWorth = 100.45;
+
+    public char herosLetter = 'B';
+
+    public bool isHeroInv = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
