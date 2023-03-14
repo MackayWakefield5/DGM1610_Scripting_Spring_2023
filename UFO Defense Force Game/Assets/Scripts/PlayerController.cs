24dce49e2d934da63c22bb1487dda9e3ed7fd11d
Mackay Wakefield <10813992@uvu.edu>
2023-03-14 11:59:10 -0600

    UFO game 1

---
+++ UFO Defense Force Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+
+    public float speed = 25;
+    
+    public float xRange = 30;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Set HorizontalInput to recieve values from keyboard
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        // Moves player left and right
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        // Keep player on screen
+        // Left side wall
+        if(transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange,transform.position.y,transform.position.z);
+        }
+
+        //Right side Wall
+        if(transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange,transform.position.y,transform.position.z);
+        }
+
+    }
+}
