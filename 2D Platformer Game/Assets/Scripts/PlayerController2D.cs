952b4cd5ece8007b2c2a574a1065445b1a6e0669
Mackay Wakefield <10813992@uvu.edu>
2023-03-14 17:54:53 -0600

    2D Platformer Game 1

---
+++ 2D Platformer Game/Assets/Scripts/PlayerController2D.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2D : MonoBehaviour
+{
+    [Header("PlayerStats")]
+
+    // How fast the player is going to move
+    public float speed;
+
+    // How high the player will jump
+    public float jumpforce;
+
+    // get move input value
+    private float moveInput; 
+
+    // Player Ridgidbody
+    [Header("Rigidbody Component")]
+
+    private Rigidbody2D rb;
+
+    private bool isFacingRight = true;
+
+    // Player Jump
+    [Header("Player Jump Settings")]
+
+    private bool isGrounded = true;
+
+    public Transform groundCheck;
+
+    public float checkRadius;
+
+    public LayerMask whatIsGround;
+
+    public bool doubleJump;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Get rigidbody component reference
+      rb = GetComponent<Ridgidbody2D>();  
+    }
+
+    // Fixed update is called a fixed or set number of frames. This works best with physics based movement
+    void FixedUpdate()
+    {
+        // Check for ground
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+        // Get the horizontal Keybinding which will return a value between -1 and 1
+        moveInput = moveInput.GetAxis("Horizontal");
+
+        // Move player left and right
+        rb.velocity = new Vector2(moveInout * speed, rb.velocity.y);
+
+        // If the player is moving right but facing left flip the player right
+        if(!isFacingRight && moveInput > 0)
+        {
+            FlipPlayer();
+        }
+
+        // If the player is moving left but facing right flip the player left
+        else if(isFacingRight && moveInput < 0)
+        {
+            FlipPlayer();
+        }
+    }
+
+    void FlipPlayer()
+    {
+        isFacingRight = !isFacingRight;
+
+        // Local Variable that store localscale value
+        Vector3 scaler = transform.localScale;
+
+        // Flip the sprit graphic
+        scaler.x *= -1;
+
+        transform.localScale = scaler;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isGrounded)
+        {
+            doubleJump = true;
+        }
+
+        if(moveInput.GetKeyDown(KeyCode.Space) && !doubleJump && isGrounded)
+        {
+            // Makes player jump
+            rb.velocity = Vector2.up * jumpforce;
+
+            doubleJump = false;
+        }
+
+        else if(moveInput.GetKeyDown(KeyCode.Space) && !doubleJump && isFacingRightGrounded)
+        {
+            // Apply jumpForce to player making player jump
+            rb.velocity = vector2.up * jumpforce;
+        }
+    }
+}
